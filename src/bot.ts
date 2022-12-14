79bffa8cdb6ae313d3661662689f131587b9f549
TURMEL Kevin <kturmel@vpauto.eu>
2022-12-14 11:33:23 +0100

    Initial commit

--- src/bot.ts
+++ src/bot.ts
@@ -1,1 +1,19 @@
 import { WebhookClient } from "discord.js";
+import Fastify from "fastify";
+
+const server = Fastify({ logger: true });
+
+server.post("/github", async (req, reply) => {
+  const body = req.body;
+
+  console.log(body);
+
+  return { hello: "world" };
+});
+
+try {
+  await server.listen({ port: 4567 });
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
