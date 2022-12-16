34be8a62486ed87ad3de2f2234d355045b09c6f7
TURMEL Kevin <kturmel@vpauto.eu>
2022-12-16 09:56:15 +0100

    feat: change for some pr type

--- src/discord/github/users.ts
+++ src/discord/github/users.ts
@@ -6,7 +6,7 @@ export type GitHubUsers = "kturmel" | "pierrehenridr" | "vpa-abdel" | "rfathi";
 export const gitHubUsers: Record<GitHubUsers, User | null> = {
   kturmel: await discordClient.users.fetch("139136227256434688"),
   pierrehenridr: await discordClient.users.fetch("902105924423020584"),
-  "vpa-abdel": await discordClient.users.fetch("139136227256434688"),
+  "vpa-abdel": await discordClient.users.fetch("920903211240550441"),
   rfathi: await discordClient.users.fetch("786512517618597891"),
 };
 
